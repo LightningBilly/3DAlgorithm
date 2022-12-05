2e8b50224924c81478ff6330bc4865406e7ec46f
chenbinbin <chenbbsun@163.com>
2022-12-05 19:57:24 +0800

    feat: 添加arapdeformation

---
+++ demos/algTest/main.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include "ARAPDeformation/hw6.h"
+#include<vector>
+#include<algorithm>
+using namespace std;
+// #include "include/PolyMesh/IOManager.h"
+
+// using namespace acamcad::polymesh;
+
+int main() {
+	arap_deformation();
+	printf("ok\n");
+	return 0;
+}
